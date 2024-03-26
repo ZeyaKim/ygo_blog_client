b5c46047a0afb91badd51ca66ad926b5edd95f7c
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-27 02:43:11 +0900

    Refactor main.dart to import and use App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart'; // App 위젯이 정의된 app.dart 파일을 import 합니다.
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Stateless Widget',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true, // Material 3 디자인을 사용
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Simple Stateless App'),
-        backgroundColor: Theme.of(context).colorScheme.primary, // 앱바의 배경색 설정
-      ),
-      body: const Center(
-        child: Text(
-          'Deploy Test!',
-          style: TextStyle(fontSize: 24), // 텍스트 스타일 설정
-        ),
-      ),
-    );
-  }
+  runApp(const App()); // App 위젯을 실행합니다.
 }
