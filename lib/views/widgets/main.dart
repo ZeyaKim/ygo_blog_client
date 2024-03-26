d9c4abde8a1c68cb8d544ad14338101b1226879f
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-27 02:42:57 +0900

    Add footer, header, and main content widgets

---
+++ lib/views/widgets/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainContent extends StatelessWidget {
+  const MainContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
